4122fab63370578b448de7c15d0da08f499128f2
eloiseboudon <boudon.eloise@gmail.com>
2025-08-21 09:16:50 +0200

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,1 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:8000';
+const { protocol, hostname } = window.location;
+const defaultPort = hostname === 'localhost' ? '8000' : '8001';
+export const API_BASE =
+    import.meta.env.VITE_API_BASE || `${protocol}//${hostname}:${defaultPort}`;
