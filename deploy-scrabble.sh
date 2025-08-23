b0ff9a64387c386f7f5fde9b348fca53926be562
eloiseboudon <boudon.eloise@gmail.com>
2025-08-23 11:38:23 +0200

    Update deploy-scrabble.sh

--- deploy-scrabble.sh
+++ deploy-scrabble.sh
@@ -698,6 +698,21 @@ server {
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
+    location = /validate {
+        proxy_pass http://localhost:$BACKEND_PORT;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+    location = /finish {
+        proxy_pass http://localhost:$BACKEND_PORT;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+
     location ^~ /games {
         proxy_pass http://localhost:$BACKEND_PORT;
         proxy_set_header Host \$host;
